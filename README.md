855c067f40e5b31f29e08162e2fe679f1adcd70a
Pranoti Dhabu <pranoti.dhabu@gmail.com>
2021-10-09 20:37:39 -0400

    Committing Assignment 3 code changes and report

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # INFO6205_Assigments
 Repository for INFO6205 course assignments
+
+Assignment 3 
+ - Modified files
+   1. HWQUPC_Solution.java
+   2. UF_HWQUPC.java
+ Path to report:
+   ./Assignment3
+
--- src/main/java/edu/neu/coe/info6205/union_find/HWQUPC_Solution.java
+++ src/main/java/edu/neu/coe/info6205/union_find/HWQUPC_Solution.java
@@ -1,4 +1,37 @@
 package edu.neu.coe.info6205.union_find;
 
+import java.util.Arrays;
+
 public class HWQUPC_Solution {
+    public static void main(String[] args) {
+        //if (args.length == 0)
+        //    throw new RuntimeException("Syntax: RandomWalk steps [experiments]");
+        for(int n = 16; n <= 262144; n=n*2) {
+            int i = 0;
+            int nPairs[] = new int[10];
+            while (i < 10) {
+                //final int nConn = count(Integer.parseInt(args[0]));
+                final int nConn = count(n);
+                //System.out.println("Number of random connections generated to connect all elements: " + nConn);
+                nPairs[i] = nConn;
+                i++;
+            }
+            System.out.println("For N: " + n + " Average: " + Arrays.stream(nPairs).average());
+        }
+    }
+
+    public static int count(int n) {
+        UF h = new UF_HWQUPC(n, true);
+        int loopCtr = 0;
+        int num1, num2;
+        while(h.components() > 1) {
+            num1 = (int) (Math.random() * n);
+            num2 = (int) (Math.random() * n);
+            if (!h.isConnected(num1, num2)) {
+                h.union(num1, num2);
+            }
+            loopCtr++;
+        }
+        return loopCtr;
+    }
 }
